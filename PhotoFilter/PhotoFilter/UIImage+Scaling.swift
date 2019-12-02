da5edabc071ea79468db301d8b611864ffcddebc
Paul Solt <PaulSolt@gmail.com>
2019-12-02 11:17:29 -0500

    Added initial project

---
+++ PhotoFilter/PhotoFilter/UIImage+Scaling.swift
@@ -0,0 +1,37 @@
+//
+//  UIImage+Scaling.swift
+//  iOS8-PhotoFilter
+//
+//  Created by Paul Solt on 9/30/19.
+//  Copyright © 2019 Lambda, Inc. All rights reserved.
+//
+
+import UIKit
+import ImageIO
+
+extension UIImage {
+    
+    /// Resize the image to a max dimension from size parameter
+    func imageByScaling(toSize size: CGSize) -> UIImage? {
+        
+        guard let data = flattened.pngData(),
+            let imageSource = CGImageSourceCreateWithData(data as CFData, nil) else {
+                return nil
+        }
+        
+        let options: [CFString: Any] = [
+            kCGImageSourceThumbnailMaxPixelSize: max(size.width, size.height),
+            kCGImageSourceCreateThumbnailFromImageAlways: true
+        ]
+        
+        return CGImageSourceCreateThumbnailAtIndex(imageSource, 0, options as CFDictionary).flatMap { UIImage(cgImage: $0) }
+    }
+    
+    /// Renders the image if the pixel data was rotated due to orientation of camera
+    var flattened: UIImage {
+        if imageOrientation == .up { return self }
+        return UIGraphicsImageRenderer(size: size, format: imageRendererFormat).image { context in
+            draw(at: .zero)
+        }
+    }
+}
---
+++ PhotoFilter/PhotoFilter/ViewController.swift
@@ -0,0 +1,44 @@
+import UIKit
+import CoreImage
+import Photos
+
+class ViewController: UIViewController {
+
+	@IBOutlet var brightnessSlider: UISlider!
+	@IBOutlet var contrastSlider: UISlider!
+	@IBOutlet var saturationSlider: UISlider!
+	@IBOutlet var imageView: UIImageView!
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+	}
+	
+	// MARK: Actions
+	
+	@IBAction func choosePhotoButtonPressed(_ sender: Any) {
+		// TODO: show the photo picker so we can choose on-device photos
+		// UIImagePickerController + Delegate
+	}
+	
+	@IBAction func savePhotoButtonPressed(_ sender: UIButton) {
+
+
+	}
+	
+
+	// MARK: Slider events
+	
+	@IBAction func brightnessChanged(_ sender: UISlider) {
+
+	}
+	
+	@IBAction func contrastChanged(_ sender: Any) {
+
+	}
+	
+	@IBAction func saturationChanged(_ sender: Any) {
+
+	}
+}
+
