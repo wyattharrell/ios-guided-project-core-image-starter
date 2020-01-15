70c397f788003d9f42b0f2ba688b43960f6685c2
Dimitri Bouniol <dimitri.bouniol@lambdaschool.com>
2020-01-14 18:22:31 -0800

    Updated image scaling to use UIKit rather than ImageIO

--- PhotoFilter/UIImage+Scaling.swift
+++ PhotoFilter/UIImage+Scaling.swift
@@ -7,24 +7,25 @@
 //
 
 import UIKit
-import ImageIO
 
 extension UIImage {
     
     /// Resize the image to a max dimension from size parameter
     func imageByScaling(toSize size: CGSize) -> UIImage? {
+        guard size.width > 0 && size.height > 0 else { return nil }
         
-        guard let data = flattened.pngData(),
-            let imageSource = CGImageSourceCreateWithData(data as CFData, nil) else {
-                return nil
+        let originalAspectRatio = self.size.width/self.size.height
+        var correctedSize = size
+        
+        if correctedSize.height > correctedSize.width*originalAspectRatio {
+            correctedSize.height = correctedSize.width*originalAspectRatio
+        } else {
+            correctedSize.width = correctedSize.height/originalAspectRatio
         }
         
-        let options: [CFString: Any] = [
-            kCGImageSourceThumbnailMaxPixelSize: max(size.width, size.height),
-            kCGImageSourceCreateThumbnailFromImageAlways: true
-        ]
-        
-        return CGImageSourceCreateThumbnailAtIndex(imageSource, 0, options as CFDictionary).flatMap { UIImage(cgImage: $0) }
+        return UIGraphicsImageRenderer(size: correctedSize, format: imageRendererFormat).image { context in
+            draw(in: CGRect(origin: .zero, size: correctedSize))
+        }
     }
     
     /// Renders the image if the pixel data was rotated due to orientation of camera
